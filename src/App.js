1278a07e971abc88c5fe19bb82cfc28e302156a7
md saheb ali <saheb.co21@gmail.com>
2021-12-23 07:35:10 +0530

    react-bootstarp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
+
+import { useEffect, useState } from 'react';
+import { Card, Col, Row, Spinner } from 'react-bootstrap';
 import './App.css';
+import News from './components/News/News';
+
+
 
 function App() {
+  const[news,setNews] = useState([]);
+  useEffect(()=>{
+    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-11-22&sortBy=publishedAt&apiKey=894038b990aa495397ade83cd4178534')
+    .then(res => res.json())
+    .then(data => setNews(data.articles))
+  },[])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="app">
+{news.length===0 ?<Spinner animation="border" /> :
+<Row xs={1} md={3} className="g-4">
+ {
+   news.map(nw => <News news={nw}></News>)
+ }
+</Row>}
+</div>
   );
 }
-
+ 
 export default App;
