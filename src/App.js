e5036f81e4bb01d9d9616fd715f293f4ab2bd070
md saheb ali <saheb.co21@gmail.com>
2022-01-02 22:45:08 +0530

    explore-react-bootstarp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-
-
 import { useEffect, useState } from 'react';
-import { Card, Col, Row, Spinner } from 'react-bootstrap';
+import {Row, Spinner } from 'react-bootstrap';
 import './App.css';
 import News from './components/News/News';
 
@@ -10,14 +8,14 @@ import News from './components/News/News';
 function App() {
   const[news,setNews] = useState([]);
   useEffect(()=>{
-    fetch('https://newsapi.org/v2/everything?q=tesla&from=2021-11-22&sortBy=publishedAt&apiKey=894038b990aa495397ade83cd4178534')
+    fetch(' https://newsapi.org/v2/everything?q=bitcoin&apiKey=894038b990aa495397ade83cd4178534')
     .then(res => res.json())
     .then(data => setNews(data.articles))
   },[])
  
   return (
 <div className="app">
-{news.length===0 ?<Spinner animation="border" /> :
+{news.length===0 ? <Spinner animation="border" />:
 <Row xs={1} md={3} className="g-4">
  {
    news.map(nw => <News news={nw}></News>)
